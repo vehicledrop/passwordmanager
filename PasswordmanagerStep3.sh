a00a69d2ee412ae0c1569468ac53ffcac418b908
vehicledrop <vehicledrop+github@gmail.com>
2024-04-28 16:19:34 +0900

    更新:PasswordmanagerStep2.sh, PasswordmanagerStep3.sh

--- PasswordmanagerStep3.sh
+++ PasswordmanagerStep3.sh
@@ -13,7 +13,7 @@ while true #正しいコマンドを入力しない場合には正しいコマ�
 do
     read answer
 
-    if [ "$answer" = "$add" ]; then #ユーザーの入力したコマンドがAdd Passwordの場合の処理
+    if [ "$answer" = "$add" ]; then #Add Passwordの場合の処理
         echo "サービス名を入力してください"
         read service
         echo "ユーザー名を入力してください"
@@ -25,26 +25,24 @@ do
             read gpgname
         fi
         if test -f "$passfile"; then #パスワードを保存した暗号化ファイルがあるときの処理(新規作成じゃない時)
-            gpgfile=$(gpg -r "$gpgname" -d "$passfile") #暗号化したパスワードファイルを復号化し変数に代入する
+            gpgfile=$(gpg -r "$gpgname" -d "$passfile") #暗号化したパスワードファイルを取得し変数に代入する
             gpgfile="$gpgfile\n$service:$username:$password" #ユーザーが入力した値をパスワードの入った変数に追加する
-            echo "パスワードファイルがある場合の処理：１"
         else #パスワードを保存した暗号化ファイルがない時（新規作成の時)
-            touch "$passfile" #空のpassword.ascファイルを作成(password.ascがない場合、下の処理にエラーが出るためエラー回避)
+            touch "$passfile" #空のpassword.ascファイルを作成(password.ascがない場合のエラー回避)
             gpgfile="$service:$username:$password" #ユーザーの入力したサービス名、ユーザー名、パスワードを保存
-            echo "パスワードファイルがない場合の処理：２"
         fi
-        echo -e "$gpgfile" | gpg -r "$gpgname" -ea > "$passfile" #変数gpgfile(ユーザーが入力した新規の値を含む)の中身を追記して保存
+        echo -e "$gpgfile" | gpg -r "$gpgname" -ea > "$passfile" #復号化したパスワードとユーザーが入力したパスワードを上書き保存
         echo "パスワードの追加は成功しました"
         echo "次の選択肢から入力してください("$add"/"$get"/"$exit")"
-    elif [ "$answer" = "$get" ]; then #ユーザーの入力したコマンドがGet Passwordの場合の処理
+    elif [ "$answer" = "$get" ]; then #Get Passwordの場合の処理
         echo "サービス名を入力してください"
         read service
         if test -z $gpgname; then #パスワードを保存した暗号化ファイルがあるときの処理(新規作成じゃない時)
             echo "GnuPGのユーザー名を入力してください"
             read gpgname
         fi
-        flag=0
-        if test -f "$passfile"; then #パスワードファイルがあるか確認(エラー回避目的)
+        flag=0 #フラグ用変数を初期化(サービスが見つからなかった場合の処理に使用)
+        if test -f "$passfile"; then #パスワードファイルがあるか確認(エラー回避)
             #gpg -r "$gpgname" -d "$passfile" | 
             while read line #復号化した内容を1行ずつ読み取り、変数lineに代入
             do
@@ -54,14 +52,14 @@ do
                     flag=1
                 fi
             done < <(gpg -r "$gpgname" -d "$passfile" | grep "^$service:")
-            if [ $flag -eq 0 ]; then
-                echo "そのサービスは登録されていません。：２"
+            if [ $flag -eq 0 ]; then #ユーザーが入力したサービス名が見つかったかどうかの判定
+                echo "そのサービスは登録されていません。"
             fi
         else #パスワードファイルがない場合、ユーザーにわかるように通知する
             echo "パスワードが保存されていないため、パスワードを取得できませんでした。"
         fi
         echo "次の選択肢から入力してください("$add"/"$get"/"$exit")"
-    elif [ "$answer" = "$exit" ]; then #ユーザーの入力したコマンドがExitの場合の処理
+    elif [ "$answer" = "$exit" ]; then #Exitの場合の処理
         echo "Thank you!"
         break
     else
